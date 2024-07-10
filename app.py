a2f1d4144272f7608713728df33787bc077141a5
Kanav-C <127048748+Kanav-C@users.noreply.github.com>
2024-07-10 14:12:01 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,15 +1,9 @@
 from langchain.vectorstores import Qdrant
-from langchain.embeddings import HuggingFaceBgeEmbeddings
+from langchain.embeddings import SentenceTransformerEmbeddings
 from qdrant_client import QdrantClient
 
-model_name = "BAAI/bge-large-en"
-model_kwargs = {'device': 'cpu'}
-encode_kwargs = {'normalize_embeddings': False}
-embeddings = HuggingFaceBgeEmbeddings(
-    model_name=model_name,
-    model_kwargs=model_kwargs,
-    encode_kwargs=encode_kwargs
-)
+model_name = "sentence-transformers/all-MiniLM-L6-v2"
+embeddings = SentenceTransformerEmbeddings(model_name=model_name)
 
 url = "http://localhost:6333"
 
@@ -20,13 +14,13 @@ client = QdrantClient(
 print(client)
 print("##############")
 
-db = Qdrant(client=client, embeddings=embeddings, collection_name="vector_db")
+db = Qdrant(client=client, embeddings=embeddings, collection_name="v_db")
 
 print(db)
 print("######")
+
 query = "How has the COVID-19 pandemic impacted the academic performance, mental health, and social development of students across different educational levels?"
 
 docs = db.similarity_search_with_score(query=query, k=5)
-for i in docs:
-    doc, score = i
+for doc, score in docs:
     print({"score": score, "content": doc.page_content, "metadata": doc.metadata})
--- ingest.py
+++ ingest.py
@@ -1,29 +1,26 @@
 from langchain.vectorstores import Qdrant
-from langchain.embeddings import HuggingFaceBgeEmbeddings
+from langchain.embeddings import SentenceTransformerEmbeddings
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.document_loaders import PyPDFLoader
 
 loader = PyPDFLoader("data.pdf")
 documents = loader.load()
+
 text_splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=50)
 texts = text_splitter.split_documents(documents)
 
-model_name = "BAAI/bge-large-en"
-model_kwargs = {'device': 'cpu'}
-encode_kwargs = {'normalize_embeddings': False}
-embeddings = HuggingFaceBgeEmbeddings(
-    model_name=model_name,
-    model_kwargs=model_kwargs,
-    encode_kwargs=encode_kwargs
-)
+model_name = "sentence-transformers/all-MiniLM-L6-v2"
+embeddings = SentenceTransformerEmbeddings(model_name=model_name)
+
 print('Embedding Model Loading...')
 url = "http://localhost:6333"
+
 qdrant = Qdrant.from_documents(
-    documents = texts,
-    embedding = embeddings,
+    documents=texts,
+    embedding=embeddings,
     url=url,
     prefer_grpc=False,
-    collection_name="vector_db"
+    collection_name="v_db"
 )
 
 print("Vector DB Successfully Created!")
